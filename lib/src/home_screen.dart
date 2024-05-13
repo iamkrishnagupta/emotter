5a6042db287d4456b4f1cbff8c48eda08cbba617
Krishna Gupta <krishna.ghhh@gmail.com>
2024-05-13 19:23:05 +0530

    Fixed Issues- Last working commit

--- lib/src/home_screen.dart
+++ lib/src/home_screen.dart
@@ -11,6 +11,11 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  CameraImage? cameraImage;
+  CameraController? cameraController;
+  String predictionResult = '';
+  bool isFrontCamera = true;
+
   @override
   void initState() {
     super.initState();
@@ -23,90 +28,75 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Emotter'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.switch_camera),
+            onPressed: _toggleCamera,
+          ),
+        ],
       ),
       body: Column(
         children: [
-          Padding(
-            padding: const EdgeInsets.all(20),
-            child: SizedBox(
-              height: MediaQuery.of(context).size.height * .5,
-              width: MediaQuery.of(context).size.width,
-              child: !cameraController!.value.isInitialized
-                  ? Container()
-                  : Padding(
-                      padding: const EdgeInsets.only(top: 20.0),
-                      child: AspectRatio(
-                        aspectRatio: cameraController!.value.aspectRatio,
-                        child: CameraPreview(cameraController!),
-                      ),
-                    ),
-            ),
+          Expanded(
+            child: !cameraController!.value.isInitialized
+                ? const Center(child: CircularProgressIndicator())
+                : AspectRatio(
+                    aspectRatio: cameraController!.value.aspectRatio,
+                    child: CameraPreview(cameraController!),
+                  ),
           ),
+          const SizedBox(height: 20),
           Text(
             'Your face seems to be $predictionResult',
-            style: const TextStyle(fontWeight: FontWeight.w500, fontSize: 20),
+            style: const TextStyle(
+              fontWeight: FontWeight.w500,
+              fontSize: 20,
+            ),
           ),
+          const SizedBox(height: 20),
         ],
       ),
     );
   }
 
-  CameraImage? cameraImage;
-  CameraController? cameraController;
-  String predictionResult = '';
-
-  //load model function
   _loadModel() async {
     await Tflite.loadModel(
-        model: 'assets/model.tflite', labels: 'assets/labels.txt');
+      model: 'assets/model.tflite',
+      labels: 'assets/labels.txt',
+    );
   }
 
-  //load camera function
   _loadCamera() {
-    // front camera description
     final frontCamera = cameraList!.firstWhere(
       (camera) => camera.lensDirection == CameraLensDirection.front,
     );
 
-    // initialize the CameraController with the front camera
     cameraController = CameraController(
-      frontCamera,
+      isFrontCamera ? frontCamera : cameraList![0],
       ResolutionPreset.medium,
     );
-
-    //in case if you wanna choose back camera
-    // cameraController = CameraController(cameraList![0], ResolutionPreset.medium); 
 
     cameraController!.initialize().then((value) {
       if (!mounted) return;
       setState(() {
         cameraController!.startImageStream((image) {
-          //whatever image we're getting from camera will be passed to our var
           cameraImage = image;
-          //next we gotta run our model
           _runModel();
         });
       });
     });
   }
 
-//run model function
   _runModel() async {
     if (cameraImage != null) {
       var predictions = await Tflite.runModelOnFrame(
-        //extracts the byte data from each plane of the camera image
-        //images are often represented as a collection of planes (YUV format)
-        bytesList: cameraImage!.planes.map((plane) {
-          //TensorFlow Lite expect image data to be in the form of byte arrays
-          // mapping over each plane and extracting its byte data, we create a list of byte data representing the entire camera image
-          return plane.bytes;
-        }).toList(),
+        bytesList: cameraImage!.planes.map((plane) => plane.bytes).toList(),
         imageHeight: cameraImage!.height,
         imageWidth: cameraImage!.width,
         imageMean: 127.5,
         imageStd: 127.5,
         rotation: 190,
-        numResults: 2, //we want to get two results
+        numResults: 2,
         threshold: 0.1,
         asynch: true,
       );
@@ -117,5 +107,12 @@ class _HomeScreenState extends State<HomeScreen> {
         });
       }
     }
+  }
+
+  void _toggleCamera() {
+    setState(() {
+      isFrontCamera = !isFrontCamera;
+      _loadCamera();
+    });
   }
 }
